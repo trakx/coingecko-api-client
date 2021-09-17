e0ac4f521d6f7d78c505cb7677169f3064110637
Yurii Bartish <yurii@trakx.io>
2021-09-17 14:46:28 +0300

    added EnvFileDocumentationUpdater

---
+++ src/Trakx.CoinGecko.ApiClient.Tests/Integration/EnvFileDocumentationUpdater.cs
@@ -0,0 +1,12 @@
+﻿using Trakx.Utils.Testing;
+using Xunit.Abstractions;
+
+namespace Trakx.CoinGecko.ApiClient.Tests.Integration
+{
+    public class EnvFileDocumentationUpdater: EnvFileDocumentationUpdaterBase
+    {
+        public EnvFileDocumentationUpdater(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+}
