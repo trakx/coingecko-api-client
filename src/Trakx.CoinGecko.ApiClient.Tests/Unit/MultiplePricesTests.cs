f24a2166a148735839768279cdcebc43e0f49c43
Emanuel Gaspar <emanuel@cognip.com>
2023-06-23 22:10:08 +0100

    Apply suggestions from code review

--- src/Trakx.CoinGecko.ApiClient.Tests/Unit/MultiplePricesTests.cs
+++ src/Trakx.CoinGecko.ApiClient.Tests/Unit/MultiplePricesTests.cs
@@ -7,20 +7,22 @@ namespace Trakx.CoinGecko.ApiClient.Tests.Unit;
 
 public class MultiplePricesTests
 {
+    private const string Coin1 = "coin1";
+
     [Fact]
     public void GetPrice_returns_direct_price()
     {
         const string mainCurrency = "eur";
 
         var source = MakePriceBag();
-        source["coin1"] = MakeDecimalBag();
+        source[Coin1] = MakeDecimalBag();
         source["coin2"] = MakeDecimalBag();
-        source["coin1"][mainCurrency] = 100m;
+        source[Coin1][mainCurrency] = 100m;
         source["coin2"][mainCurrency] = 200m;
 
         var prices = new MultiplePrices(source);
 
-        var price = prices.GetPrice("coin1", mainCurrency);
+        var price = prices.GetPrice(Coin1, mainCurrency);
         price.Should().Be(100m);
     }
 
@@ -40,16 +42,16 @@ public class MultiplePricesTests
         var expectedPrice = basePriceInMainCurrency * rateBetweenMainAndWanted;
 
         var source = MakePriceBag();
-        source["coin1"] = MakeDecimalBag();
+        source[Coin1] = MakeDecimalBag();
         source["coin2"] = MakeDecimalBag();
         source[wantedQuoteCurrency] = MakeDecimalBag();
 
-        source["coin1"][mainCurrency] = basePriceInMainCurrency;
+        source[Coin1][mainCurrency] = basePriceInMainCurrency;
         source[wantedQuoteCurrency][mainCurrency] = rateBetweenWantedAndMain;
 
         var prices = new MultiplePrices(source);
 
-        var price = prices.GetPrice("coin1", wantedQuoteCurrency);
+        var price = prices.GetPrice(Coin1, wantedQuoteCurrency);
         price.Should().Be(expectedPrice);
     }
 
